5cb77916cc8cf3527a84588ada56c660496e3a65
Pakas142 <120348717+Pakas142@users.noreply.github.com>
2023-08-27 23:18:47 +0530

    Delete utility directory

--- utility/utility.js
+++
@@ -1,9 +0,0 @@
-const jwt = require ('jsonwebtoken')
-
-const generateToken = (id , name ) =>{
-    return jwt.sign({id , name } , process.env.JWT_SECRET , {
-        expiresIn : '1h'
-    })
-}
-
-module.exports = generateToken
